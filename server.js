9c302d151a4b61413002f1aaaa4e4c2de1083f45
Justin-110 <tianji@ad.unc.edu>
2022-11-17 00:40:18 -0500

    A04-16

--- server.js
+++ server.js
@@ -12,52 +12,47 @@ app.use(express.urlencoded({extended:true}));
 //take an arbitrary port number as a command line argument, default to 5000 if no argument is given
 var port = 5000;
 if ("port" in args) {
-    port = args.port
+    port = args.port;
 };
 
 
 app.get('/', (req, res, next) => {
-	res.send('Hello, world!')
+	res.send('Hello, world!');
 });
 
 // Check endpoint at /app/ that returns 200 OK
 app.get('/app/', (req, res, next) => {
-    res.status(200)
-    res.send('200 OK')
+    res.status(200);
+    res.send('200 OK');
 });
-
-const sides = 6;
-const dice = 2;
-const rolls = 1;
 
 app.get('/app/roll/', (req, res) => {
-    res.status(200)
-    res.send(roll(parseInt(sides, dice, rolls)))
+    res.status(200);
+    res.send(roll(6,2,1));
 });
 
-app.post('/app/roll/', (req, res) => {
-    res.status(200);
-    res.send(roll(parseInt(req.body.sides),parseInt(req.body.dice),parseInt(req.body.rolls)));
+app.post('/app/roll/', (req,res) => {
+	res.send(roll(parseInt(req.body.sides),parseInt(req.body.dice),parseInt(req.body.rolls)));
 });
 
 app.get('/app/roll/:sides/', (req, res) => {
     res.status(200);
-    res.send(roll(parseInt(req.params.sides), dice, rolls))
+    res.send(roll(parseInt(req.params.sides), 2, 1));
 });
 
 app.get('/app/roll/:sides/:dice/', (req, res) => {
     res.status(200);
-    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), rolls))
+    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
 });
 
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
     res.status(200);
-    res.send(roll(parseInt(req.params.sides),parseInt(req.params.dice),parseInt(req.params.rolls)))
+    res.send(roll(parseInt(req.params.sides),parseInt(req.params.dice),parseInt(req.params.rolls)));
 });
 
 // Default API endpoint that returns 404 NOT FOUND for any endpoints that are not defined
 app.get("*", (req, res) => {
     res.status(404).send("404 NOT FOUND");
-  });
+});
 
 app.listen(port);
